6381c65cbce818db68b82bbe1abf3ab3bd1acb05
ljp1992 <1121403085>
2017-12-07 11:57:46 +0800

    批次号上传附件

---
+++ qdodoo_lot_attachment/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "qdodoo_lot_attachment",
+
+    'summary': """
+        批次上传附件""",
+
+    'description': """
+    """,
+
+    'author': "青岛欧度软件技术有限责任公司",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
